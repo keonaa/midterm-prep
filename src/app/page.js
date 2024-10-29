7d87b6c67961ca81a12e614bc326e2a56c0dd3ec
keonaa <keona.aguilar@gmail.com>
2024-10-29 15:36:18 -0700

    created mid term prep

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,85 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+
+//TODO
+// - add button
+// - function to fetch data
+// - function to add data to state
+// - make it responsive
+// - display data
+// - function to clear data
+// - component for empty state
+// - component for data state
+// - error handling if the data doesn't come back (bonus)
+
+//side note
+// use atomic principles when coding
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // if useState (isn't)!null, probably fetching or loading data, or have data
+  // if useState ==== data, we can display our data
+  const [astronomyData, setAstronomyData ] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  async function fetchAstronomyData() {
+    //fetchData
+    setLoading(true);
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5");
+
+    const data = await response.json();
+
+    setAstronomyData(data);
+    setLoading(false);
+  }
+
+  const DisplayAstronomyData = () => {
+    // display if we have data
+    // loading state
+    // fulfilled state (data exists)
+    // empty state (!data)
+
+    // let formattedData = JSON.stringify(astronomyData);
+    if(loading) return <div>Loading</div>
+
+    if(astronomyData) {
+    const dataThatIsFormattedForDisplay = [];
+    astronomyData.forEach((entry, i) => {
+      dataThatIsFormattedForDisplay.push(
+      <article key={i}>
+        <img src={entry.url}/>
+        <h3>{entry.title}</h3>
+        <p>{entry.explanation}</p>
+      </article>
+      );
+
+    })
+    return <section>{dataThatIsFormattedForDisplay}</section>;
+
+  }
+    return (<div>Empty, no data fetched</div>)
+  };
+
+  const Header = () => {
+    // build the UI that grabs data
+    return (
+    <header>
+      welcome to midterm prep!
+      <br/>
+      <button 
+        onClick={fetchAstronomyData} 
+        className="border-black border-solid border-2 p-2 ">
+          fetch stuff 
+      </button>
+    </header>
+    );
+  };
+
+
+  return (
+    <div className='m-8'>
+      <Header />
+      <DisplayAstronomyData />
+   </div>
   );
+
 }
